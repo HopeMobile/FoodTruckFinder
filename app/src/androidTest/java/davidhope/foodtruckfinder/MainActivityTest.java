82b65b3acc37320e0cd30d74ef44ad4f8b75ae2b
HopeMobile <vonmisesdawg@gmail.com>
2015-12-18 17:37:16 -0500

    Initial commit

---
+++ app/src/androidTest/java/davidhope/foodtruckfinder/MainActivityTest.java
@@ -0,0 +1,23 @@
+package davidhope.foodtruckfinder;
+
+// Test class for Main Activity
+@org.junit.runner.RunWith(org.robolectric.RobolectricTestRunner.class)
+ public class MainActivityTest extends MainActivity {
+
+
+
+
+
+
+
+ //@org.junit.Test public void checkSearchViewLabelExistence() throws Exception {
+     //MainActivity controller = org.robolectric.Robolectric.buildActivity(MainActivity.class).create().get();
+     //android.app.Activity activity =  controller.getParent();
+     // check if SearchView wasn't created.
+     //org.junit.Assert.assertNotNull("SearchView is not visible", SearchView.INVISIBLE);
+
+     // check if SearchView was created
+ }
+//}
+
+
